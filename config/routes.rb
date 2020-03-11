a26a45cd2062df53f84ff400702b5a0c0b11d6cd
Mikoto <suganuma3510@gmail.com>
2020-03-11 21:50:04 +0900

    view,controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  
+  get 'user' => 'users#index'
   get '/' => 'posts#index'
+  
+  devise_for :users, skip: :all
+  devise_scope :user do
+   get 'login'              => 'devise/sessions#new',          as: :new_user_session
+   post 'login'             => 'devise/sessions#create',       as: :user_session
+   delete 'logout'          => 'devise/sessions#destroy',      as: :destroy_user_session
+   get 'signup'             => 'devise/registrations#new',     as: :new_user_registration
+   post 'signup'            => 'devise/registrations#create',  as: :user_registration
+   get 'signup/cancel'      => 'devise/registrations#cancel',  as: :cancel_user_registration
+   get 'user/profile'       => 'devise/registrations#edit',    as: :edit_user_registration
+   patch 'user/profile'     => 'devise/registrations#update',  as: nil
+   put 'user/profile'       => 'devise/registrations#update',  as: :update_user_registration
+   delete 'signup'          => 'devise/registrations#destroy', as: :destroy_user_registration
+   get 'user/password'      => 'devise/passwords#new',         as: :new_user_password
+   post 'user/password'     => 'devise/passwords#create',      as: :user_password
+   get 'user/password/edit' => 'devise/passwords#edit',        as: :edit_user_password
+   patch 'user/password'    => 'devise/passwords#update'
+   put 'user/password'      => 'devise/passwords#update',      as: :update_user_password
+  end
   
 end
