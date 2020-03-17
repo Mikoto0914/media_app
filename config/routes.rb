06cde059a3bd315a6daa45ebdab43678be2ecdaa
Mikoto <suganuma3510@gmail.com>
2020-03-18 04:14:07 +0900

    プロフィール欄追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  get '/' => 'posts#index'
+  get  'users/profile_edit' => 'users/profile_edit'
+  get  'users/:id'          => 'users#show'
   
-  get  'posts/new'        => 'posts#new'
-  get  'posts/:id'        => 'posts#show'
-  post 'posts/create'     => 'posts#create'
-  get  'posts/:id/edit'   => 'posts#edit'
-  post 'posts/:id/update' => 'posts#update'
-  post 'posts/:id/destroy' => 'posts#destroy'
-  get  'users/profile_edit'=>  'users/profile_edit'
-  get  'users/:id'         => 'users#show'
+  get  '/'                  => 'posts#index'
+  get  'posts/new'          => 'posts#new'
+  get  'posts/:id'          => 'posts#show'
+  post 'posts/create'       => 'posts#create'
+  get  'posts/:id/edit'     => 'posts#edit'
+  post 'posts/:id/update'   => 'posts#update'
+  post 'posts/:id/destroy'  => 'posts#destroy'
+  
   
   
   
