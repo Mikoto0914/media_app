bf1f2efc4231da6a1f33e1cee04a27971825b60e
Mikoto <suganuma3510@gmail.com>
2020-03-20 04:37:56 +0900

    下書き一覧追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
   
-  before_action :authenticate_user!, only: [:profile_edit]
+  before_action :authenticate_user!, only: [:profile_edit, :drafts]
   
   def show
-    @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: params[:id])
+    @posts = Post.where(user: params[:id]).where(publish_flg: true).order(id: "DESC")
   end
   
   def profile_edit
+  end
+  
+  def drafts
+    @posts_drafts = Post.where(user: current_user.id).where(publish_flg: false).order(id: "DESC")
+    @posts_releas = Post.where(user: current_user.id).where(publish_flg: true).order(id: "DESC")
   end
 
 end
