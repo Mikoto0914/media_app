941cb0e4ed505195832875f5c22c1d22251cccd2
Mikoto <suganuma3510@gmail.com>
2020-03-23 05:08:10 +0900

    stockモデル追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,25 @@
 class Post < ApplicationRecord
   
   belongs_to :user
+  has_many :stocks
+  has_many :stock_users, through: :stocks, source: :user
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
+  
+  # 現在ログインしているユーザーidを受け取り、記事をストックする
+  def stock(user)
+    stocks.create(user_id: user.id)
+  end
+
+  # 現在ログインしているユーザーidを受け取り、記事のストックを解除する
+  def unstock(user)
+    stocks.find_by(user_id: user.id).destroy
+  end
+
+  # 記事がストック済みであるかを判定
+  # 取得済みであれば true を返す
+  def stocked?(user)
+    stock_users.include?(user)
+  end
   
 end
