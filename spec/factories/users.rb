d3f7964390a367d262d6145da41b543954b8a1d6
Mikoto <suganuma3510@gmail.com>
2020-04-22 00:04:14 +0900

    user_controllerテスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     password { "password" }
     password_confirmation { "password" }
     email { "test@example.com" }
+    image { open("#{Rails.root}/public/admin.png") }
+    profile { "管理人：本サービスで登録されたユーザの情報を確認、編集ができる。"}
   end
 end
