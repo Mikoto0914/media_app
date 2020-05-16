629ac5ba9f3c4c6d36132b0ab81afbddb7b901d0
Mikoto <suganuma3510@gmail.com>
2020-05-17 03:48:06 +0900

    user統合テスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     email { "test@example.com" }
     image { open("#{Rails.root}/public/admin.png") }
     profile { "管理人：本サービスで登録されたユーザの情報を確認、編集ができる。"}
-<<<<<<< HEAD
   end
  
   factory :user1, class: User do
@@ -21,7 +20,5 @@ FactoryBot.define do
     password { "password" }
     password_confirmation { "password" }
     email { "user2@example.com" }
-=======
->>>>>>> develop
   end
 end
---
+++ spec/features/user_spec.rb
@@ -0,0 +1,105 @@
+require "rails_helper"
+
+feature "user", type: :feature do
+
+  feature "ユーザー登録前" do
+    scenario "新規登録できるか" do
+      visit root_path
+      expect {
+        click_on("新規登録")
+        fill_in "user_name", with: "test0"
+        fill_in "user_email", with: "test0@example.com"
+        fill_in "user_password", with: "password"
+        fill_in "user_password_confirmation", with: "password"
+        click_on("登録する")
+      }.to change{ User.count }.by(1)
+    end
+    
+    scenario "ログインできないか" do
+      visit root_path
+      click_on("ログイン")
+      fill_in "user_email", with: "test0@example.com"
+      fill_in "user_password", with: "password"
+      click_on("ログインする")
+      expect(page).to have_no_content "ログアウト"
+    end
+  end
+    
+  feature "ユーザー登録後" do
+    given(:user) { create(:user) }
+    
+    background  do
+      visit new_user_session_path
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: user.password
+      click_on("ログインする")
+    end
+    
+    scenario "ログインできるか" do
+      expect(page).to have_content("ログアウト")
+    end
+    
+    scenario "マイページへ遷移できるか" do
+      visit root_path
+      click_on("arrow_drop_down")
+      click_on("マイページ")
+      expect(current_path).to eq "/users/#{user.id}"
+    end
+    
+    scenario "名前を変更できるか" do
+      click_on("arrow_drop_down")
+      click_on("アカウント設定")
+      click_on("プロフィール編集")
+      fill_in "user_name", with: "newname"
+      click_on("変更する")
+      visit users_profile_edit_path
+      expect(page).to have_content("newname")
+    end
+      
+    scenario "アイコンを変更できるか" do
+      click_on("arrow_drop_down")
+      click_on("アカウント設定")
+      click_on("プロフィール編集")
+      attach_file("user_image", "#{Rails.root}/public/admin.png")
+      click_on("変更する")
+      visit users_profile_edit_path
+      expect(page).to have_selector("img[src$="admin.png"]")
+    end
+    
+    scenario "プロフィールを変更できるか" do
+      click_on("arrow_drop_down")
+      click_on("アカウント設定")
+      click_on("プロフィール編集")
+      fill_in "user_profile", with: "profile-test"
+      click_on("変更する")
+      visit users_profile_edit_path
+      expect(page).to have_content("profile-test")
+    end
+    
+    scenario "パスワードを変更できるか" do
+      click_on("arrow_drop_down")
+      click_on("アカウント設定")
+      fill_in "user_password", with: "123456"
+      fill_in "user_current_password", with: "password"
+      click_on("パスワードを変更する")
+      expect(current_path).to eq root_path
+    end
+  
+    scenario "アカウント削除できるか" do
+      visit "/users/edit"
+      expect {
+        click_on("削除する")
+        click_on("はい")
+      }.to change{ User.count }.by(-1)
+    end
+  end
+  
+  feature "ユーザー共通操作" do
+    given(:user1) { create(:user1) }
+    
+    scenario "他ユーザーのマイページへ遷移できるか" do
+      visit "/users/#{user1.id}"
+      expect(page).to have_content(user1.name)
+    end
+  end
+end
