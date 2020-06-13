de5527378f11f75a9f693af50605e6cd27acea09
Mikoto <suganuma3510@gmail.com>
2020-06-13 11:36:43 +0900

    タグ検索機能追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,4 @@
 class Tag < ApplicationRecord
   has_many :tag_maps, dependent: :destroy
   has_many :posts,    through:   :tag_maps
-  
-  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
 end
