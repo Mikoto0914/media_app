1f67c826d62f6ca37dc3f7de1b54e6edd8f56030
Mikoto <suganuma3510@gmail.com>
2020-07-04 22:33:32 +0900

    コメント削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,11 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    Comment.find_by(id: params[:id],post_id: params[:post_id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content)
