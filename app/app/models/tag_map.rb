d1ccdaf99faca97c42a2e37328575ca8c99213b1
suganuma3510 <suganuma3510@gmail.com>
2022-04-04 10:08:58 +0900

    clean：不要なファイルを削除

--- app/app/models/tag_map.rb
+++
@@ -1,9 +0,0 @@
-class TagMap < ApplicationRecord
-  belongs_to :post
-  belongs_to :tag
-  
-  #タグのランキング（最大５件）
-  def self.create_tag_ranks
-    TagMap.group(:tag_id).limit(5).order('count(tag_id) desc').count
-  end
-end
