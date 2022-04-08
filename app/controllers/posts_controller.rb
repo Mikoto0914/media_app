51584a7dca2b33ea58eaa91a5543273b6cf3471f
suganuma3510 <suganuma3510@gmail.com>
2022-04-08 23:57:45 +0900

    add：S3の書名付きURLを発行する処理を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -98,4 +98,14 @@ class PostsController < ApplicationController
   def set_target_post
     @post = Post.find(params[:id])
   end
+
+  def s3_direct_post
+    resource = S3_BUCKET.presigned_post(
+      key: "uploads/post/#{SecureRandom.uuid}/${filename}",
+      success_action_status: '201',
+      acl: 'public-read',
+      content_length_range: 1..(10.megabytes)
+    )
+    render json: { url: resource.url, fields: resource.fields }
+  end
 end
